36d339cb8f1fd5acd9981d78c62499dfa163b08b
Mihir Moolchandani <121425409+horrible-hacker@users.noreply.github.com>
2025-06-19 01:40:31 +0530

    Update deploy.js
    Some functions like deployed() and address got depriciated in new version of hardhat.
    changed it to newer function.

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,12 +1,12 @@
 const hre = require("hardhat");
 async function main()
 {
-    //const [owner] = await hre.ethers.getSigners(); // Get the deployer
+    const [owner] = await hre.ethers.getSigners(); // Get the deployer
     const Upload = await hre.ethers.getContractFactory("Upload");
     const upload = await Upload.deploy();
-    await upload.deployed();
+    await upload.waitForDeployment();
     //console.log("Deployer address:", owner.address);
-    console.log("token address:", upload.address);
+    console.log("token address:", await upload.getAddress());
 }
 main()
 .then(()=>process.exit(0))
